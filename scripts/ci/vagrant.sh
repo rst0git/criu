1c915ffe4b22af3d959089c97fb499ae858bdf43
Radostin Stoyanov <rstoyanov@fedoraproject.org>
2021-01-22 22:34:04 +0000

    test
    Signed-off-by: Radostin Stoyanov <rstoyanov@fedoraproject.org>

--- scripts/ci/vagrant.sh
+++ scripts/ci/vagrant.sh
@@ -6,38 +6,21 @@
 set -e
 set -x
 
-VAGRANT_VERSION=2.2.7
-FEDORA_VERSION=32
-FEDORA_BOX_VERSION=32.20200422.0
+FEDORA_VERSION=33
+FEDORA_BOX_VERSION=33.20201019.0
 
 setup() {
-	apt-get -qq update
-	# Load the kvm modules for vagrant to use qemu
-	modprobe kvm kvm_intel
-
-	# Tar up the git checkout to have vagrant rsync it to the VM
-	tar cf criu.tar ../../../criu
-	wget https://releases.hashicorp.com/vagrant/${VAGRANT_VERSION}/vagrant_${VAGRANT_VERSION}_"$(uname -m)".deb -O /tmp/vagrant.deb && \
-		dpkg -i /tmp/vagrant.deb
-
-	./apt-install libvirt-bin libvirt-dev qemu-utils qemu
-	systemctl restart libvirt-bin
-	vagrant plugin install vagrant-libvirt
 	vagrant init fedora/${FEDORA_VERSION}-cloud-base --box-version ${FEDORA_BOX_VERSION}
-	# The default libvirt Vagrant VM uses 512MB.
-	# Travis VMs should have around 7.5GB.
-	# Increasing it to 4GB should work.
-	sed -i Vagrantfile -e 's,^end$,  config.vm.provider :libvirt do |libvirt|'"\n"'    libvirt.memory = 4096;end'"\n"'end,g'
-	vagrant up --provider=libvirt
-	mkdir -p /root/.ssh
-	vagrant ssh-config >> /root/.ssh/config
-	ssh default sudo dnf install -y gcc git gnutls-devel nftables-devel libaio-devel \
+	vagrant up --no-tty
+	mkdir -p ~/.ssh
+	vagrant ssh-config >> ~/.ssh/config
+	ssh -i .vagrant/machines/default/virtualbox/private_key -R default vagrant@127.0.0.1 -p 2222 sudo dnf install -y gcc git gnutls-devel nftables-devel libaio-devel \
 		libasan libcap-devel libnet-devel libnl3-devel make protobuf-c-devel \
 		protobuf-devel python3-flake8 python3-future python3-protobuf \
 		python3-junit_xml rubygem-asciidoctor iptables libselinux-devel libbpf-devel
 	# Disable sssd to avoid zdtm test failures in pty04 due to sssd socket
-	ssh default sudo systemctl mask sssd
-	ssh default cat /proc/cmdline
+        ssh -i .vagrant/machines/default/virtualbox/private_key -R default vagrant@127.0.0.1 -p 2222 sudo systemctl mask sssd
+        ssh -i .vagrant/machines/default/virtualbox/private_key -R default vagrant@127.0.0.1 -p 2222 cat /proc/cmdline
 }
 
 fedora-no-vdso() {
