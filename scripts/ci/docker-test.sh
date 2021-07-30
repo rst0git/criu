42b83092227c3b56730160156ee5c07ee8d9dd00
Radostin Stoyanov <rstoyanov@fedoraproject.org>
2021-07-30 07:24:13 +0100

    docker-test: remove container after restore
    In the current test for docker, we create a single container that is
    checkpoint/restore it 50 times sequentially. It is fairly uncommon
    (and not very useful) to checkpoint a container immediately after
    restore.
    
    We have seen the following error from containerd that occurs when trying
    to checkpoint a container immediately after restore:
    
    Error response from daemon: Cannot checkpoint container cr: content sha256:12c69b7a9d25695dd5f9d37d4e858e2f7c3f9da738ccf86f8d3042f6973af1df: already exists
    
    This patch aims to address the error mentioned above by modifying the
    test to remove the container after restore. Thus, it would use a new
    container for each checkpoint/restore test iteration.
    
    Signed-off-by: Radostin Stoyanov <rstoyanov@fedoraproject.org>

--- scripts/ci/docker-test.sh
+++ scripts/ci/docker-test.sh
@@ -51,20 +51,19 @@ docker info
 
 criu --version
 
-# shellcheck disable=SC2016
-docker run --tmpfs /tmp --tmpfs /run --read-only --security-opt seccomp=unconfined --name cr -d alpine /bin/sh -c 'i=0; while true; do echo $i; i=$(expr $i + 1); sleep 1; done'
+# Scenario : Create multiple containers and checkpoint and restore them once
+for i in $(seq 10); do
+	docker run --tmpfs /tmp --tmpfs /run --read-only --security-opt seccomp=unconfined --name cr -d alpine /bin/sh -c 'i=0; while true; do echo $i; i=$(expr $i + 1); sleep 1; done'
+	# wait when docker updates a container state
+	until [ "$(docker inspect -f '{{.State.Running}}' cr)" == "true" ]; do
+		sleep 1;
+	done;
 
-sleep 1
-for i in $(seq 50); do
-	# docker start returns 0 silently if a container is already started
-	# docker checkpoint doesn't wait when docker updates a container state
-	# Due to both these points, we need to sleep after docker checkpoint to
-	# avoid races with docker start.
-	docker exec cr ps axf &&
 	docker checkpoint create cr checkpoint"$i" &&
-	sleep 1 &&
-	docker ps &&
 	(docker exec cr true && exit 1 || exit 0) &&
+
+	# wait for container to stop
+	docker wait cr
 	docker start --checkpoint checkpoint"$i" cr 2>&1 | tee log || {
 	cat "$(grep log 'log file:' | sed 's/log file:\s*//')" || true
 		docker logs cr || true
@@ -74,6 +73,30 @@ for i in $(seq 50); do
 		exit 1
 	}
 	docker ps
-	sleep 1
+	docker rm -f cr
 done
 
+# Scenario: Create container and checkpoint and restore it multiple times
+docker run --tmpfs /tmp --tmpfs /run --read-only --security-opt seccomp=unconfined --name cr -d alpine /bin/sh -c 'i=0; while true; do echo $i; i=$(expr $i + 1); sleep 1; done'
+for i in $(seq 10); do
+	# wait when docker updates a container state
+	until [ "$(docker inspect -f '{{.State.Running}}' cr)" == "true" ]; do
+		sleep 1;
+	done;
+
+	docker checkpoint create cr checkpoint &&
+	(docker exec cr true && exit 1 || exit 0) &&
+	# wait for container to stop
+	docker wait cr
+
+	docker start --checkpoint checkpoint cr 2>&1 | tee log || {
+	cat "$(grep log 'log file:' | sed 's/log file:\s*//')" || true
+		docker logs cr || true
+		cat $CRIU_LOG || true
+		dmesg
+		docker ps
+		exit 1
+	}
+	docker ps
+	docker checkpoint rm cr checkpoint
+done
