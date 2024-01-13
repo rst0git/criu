8ab8d47e92218bf5856b9048201b65e9fef4bce4
Radostin Stoyanov <rstoyanov@fedoraproject.org>
2024-01-13 11:49:31 +0000

    stats: measure time for encryption/decryption
    Measure the time for data encryption and decryption with
    stream and block ciphers.
    
    Signed-off-by: Radostin Stoyanov <rstoyanov@fedoraproject.org>

--- criu/cr-restore.c
+++ criu/cr-restore.c
@@ -2359,13 +2359,13 @@ int cr_restore_tasks(void)
 	if (cr_plugin_init(CR_PLUGIN_STAGE__RESTORE))
 		return -1;
 
+	if (init_stats(RESTORE_STATS))
+		goto err;
+
 	if (tls_initialize_cipher_from_image())
 		goto err;
 
 	if (check_img_inventory(/* restore = */ true) < 0)
-		goto err;
-
-	if (init_stats(RESTORE_STATS))
 		goto err;
 
 	if (lsm_check_opts())
