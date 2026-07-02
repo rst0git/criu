rst0git/criu

commit 67887251ece28a53d9ec20b6432c0474d8d4a656

compel: avoid rseq critical sections when executing parasite code
A parasite code is injected into a target process and executed it via
ptrace. If the instruction pointer is inside an rseq critical section
when parasite code starts executing, the critical section can be aborted
and IP will be redirected to rseq_cs->abort_ip, which isn't expected.

Here is one more issue is that the kernel zeroes out rseq->rseq_cs in
userspace memory, destroying the rseq state before CRIU can record it.

To avoid critical sections during parasite execution, compel aborts a
critical section when it is required.

Signed-off-by: Andrei Vagin <avagin@google.com>

compel/include/uapi/infect.h
@@ -47,12 +47,12 @@ extern int compel_resume_task_sig(pid_t pid, int orig_state, int state, int stop
 struct parasite_ctl;
 struct parasite_thread_ctl;
 
-extern struct parasite_ctl __must_check *compel_prepare(int pid);
-extern struct parasite_ctl __must_check *compel_prepare_noctx(int pid);
+extern struct parasite_ctl __must_check *compel_prepare(int pid, bool handle_rseq);
+extern struct parasite_ctl __must_check *compel_prepare_noctx(int pid, bool handle_rseq);
 extern int __must_check compel_infect(struct parasite_ctl *ctl, unsigned long nr_threads, unsigned long args_size);
 extern int __must_check compel_infect_no_daemon(struct parasite_ctl *ctl, unsigned long nr_threads,
 						unsigned long args_size);
-extern struct parasite_thread_ctl __must_check *compel_prepare_thread(struct parasite_ctl *ctl, int pid);
+extern struct parasite_thread_ctl __must_check *compel_prepare_thread(struct parasite_ctl *ctl, int pid, bool handle_rseq);
 extern void compel_release_thread(struct parasite_thread_ctl *);
 
 extern int __must_check compel_start_daemon(struct parasite_ctl *ctl);

compel/src/lib/infect.c
@@ -14,6 +14,7 @@
 #include "common/xmalloc.h"
 #include "common/lock.h"
 #include "common/page.h"
+#include "linux/rseq.h"
 
 #include <compel/plugins/std/syscall-codes.h>
 #include <compel/plugins/std/asm/syscall-types.h>
@@ -42,7 +43,7 @@
 #define SECCOMP_MODE_DISABLED 0
 #endif
 
-static int prepare_thread(int pid, struct thread_ctx *ctx);
+static int prepare_thread(int pid, struct thread_ctx *ctx, bool handle_rseq);
 
 static inline void close_safe(int *pfd)
 {
@@ -1111,13 +1112,13 @@ int compel_infect(struct parasite_ctl *ctl, unsigned long nr_threads, unsigned l
 	return 0;
 }
 
-struct parasite_thread_ctl *compel_prepare_thread(struct parasite_ctl *ctl, int pid)
+struct parasite_thread_ctl *compel_prepare_thread(struct parasite_ctl *ctl, int pid, bool handle_rseq)
 {
 	struct parasite_thread_ctl *tctl;
 
 	tctl = xmemalign(__alignof__(*tctl), sizeof(*tctl));
 	if (tctl) {
-		if (prepare_thread(pid, &tctl->th)) {
+		if (prepare_thread(pid, &tctl->th, handle_rseq)) {
 			xfree(tctl);
 			tctl = NULL;
 		} else {
@@ -1129,7 +1130,101 @@ struct parasite_thread_ctl *compel_prepare_thread(struct parasite_ctl *ctl, int 
 	return tctl;
 }
 
-static int prepare_thread(int pid, struct thread_ctx *ctx)
+static bool task_in_rseq(struct criu_rseq_cs *rseq_cs, uint64_t addr)
+{
+	return addr >= rseq_cs->start_ip && addr < rseq_cs->start_ip + rseq_cs->post_commit_offset;
+}
+
+static int read_rseq_cs(pid_t tid, struct __ptrace_rseq_configuration *rseqc, struct criu_rseq_cs *rseq_cs,
+			struct criu_rseq *rseq)
+{
+	int ret;
+
+	if (!rseqc->rseq_abi_pointer)
+		return 0;
+
+	ret = ptrace_peek_area(tid, rseq, (void *)(long)(rseqc->rseq_abi_pointer), sizeof(struct criu_rseq));
+	if (ret) {
+		pr_err("ptrace_peek_area(%d, %lx, %lx, %lx): fail to read rseq struct\n", tid, (unsigned long)rseq,
+		       (unsigned long)(rseqc->rseq_abi_pointer), (unsigned long)sizeof(uint64_t));
+		return -1;
+	}
+
+	if (!rseq->rseq_cs)
+		return 0;
+
+	ret = ptrace_peek_area(tid, rseq_cs, (void *)(long)(rseq->rseq_cs), sizeof(struct criu_rseq_cs));
+	if (ret) {
+		pr_err("ptrace_peek_area(%d, %lx, %lx, %lx): fail to read rseq_cs struct\n", tid,
+		       (unsigned long)rseq_cs, (unsigned long)rseq->rseq_cs,
+		       (unsigned long)sizeof(struct criu_rseq_cs));
+		return -1;
+	}
+
+	return 0;
+}
+
+/*
+ * parasite_thread_rseq checks if the victim process is in an rseq critical
+ * section and, if so, aborts it.
+ */
+static int parasite_thread_rseq(int pid, struct thread_ctx *ctx)
+{
+	struct criu_rseq_cs _rseq_cs = {}, *rseq_cs = &_rseq_cs;
+	struct __ptrace_rseq_configuration rseqc;
+	int ret;
+	struct criu_rseq rseq = {};
+
+	ret = ptrace(PTRACE_GET_RSEQ_CONFIGURATION, pid, sizeof(rseqc), &rseqc);
+	if (ret != sizeof(rseqc)) {
+		pr_perror("ptrace(PTRACE_GET_RSEQ_CONFIGURATION, %d) = %d", pid, ret);
+		return -1;
+	}
+
+	if (rseqc.flags != 0) {
+		pr_err("something wrong with ptrace(PTRACE_GET_RSEQ_CONFIGURATION, %d) flags = 0x%x\n", pid,
+		       rseqc.flags);
+		return -1;
+	}
+
+	pr_debug("rseq of %d: ptr = 0x%lx sign = 0x%x\n", pid, (unsigned long)rseqc.rseq_abi_pointer,
+		 rseqc.signature);
+
+	if (read_rseq_cs(pid, &rseqc, rseq_cs, &rseq))
+		goto err;
+
+	if (rseq_cs->start_ip) {
+		void *zero_addr = 0;
+
+		pr_debug(
+			"fixup_thread_rseq for %d: rseq_cs start_ip = %llx abort_ip = %llx post_commit_offset = %llx flags = %x version = %x; IP = %lx\n",
+			pid, rseq_cs->start_ip, rseq_cs->abort_ip, rseq_cs->post_commit_offset, rseq_cs->flags,
+			rseq_cs->version, (unsigned long)REG_IP(ctx->regs));
+
+		if (rseq_cs->version != 0) {
+			pr_err("unsupported RSEQ ABI version = %d\n", rseq_cs->version);
+			return -1;
+		}
+
+		if (task_in_rseq(rseq_cs, REG_IP(ctx->regs)))
+			SET_REG_IP(ctx->regs, rseq_cs->abort_ip);
+
+		if (ptrace_poke_area(pid, &zero_addr,
+				     (void *)(long)(rseqc.rseq_abi_pointer) +
+					     offsetof(struct criu_rseq, rseq_cs),
+				     sizeof(zero_addr))) {
+			pr_err("ptrace_poke_area(%d) failed to zero out rseq_cs\n", pid);
+			return -1;
+		}
+	}
+
+	return 0;
+
+err:
+	return -1;
+}
+
+static int prepare_thread(int pid, struct thread_ctx *ctx, bool handle_rseq)
 {
 	if (ptrace(PTRACE_GETSIGMASK, pid, sizeof(k_rtsigset_t), &ctx->sigmask)) {
 		pr_perror("can't get signal blocking mask for %d", pid);
@@ -1138,6 +1233,11 @@ static int prepare_thread(int pid, struct thread_ctx *ctx)
 
 	if (ptrace_get_regs(pid, &ctx->regs)) {
 		pr_perror("Can't obtain registers (pid: %d)", pid);
+		return -1;
+	}
+
+	if (handle_rseq && parasite_thread_rseq(pid, ctx)) {
+		pr_err("Can't handle rseq (pid: %d)\n", pid);
 		return -1;
 	}
 
@@ -1153,7 +1253,7 @@ void compel_release_thread(struct parasite_thread_ctl *tctl)
 	xfree(tctl);
 }
 
-struct parasite_ctl *compel_prepare_noctx(int pid)
+struct parasite_ctl *compel_prepare_noctx(int pid, bool handle_rseq)
 {
 	struct parasite_ctl *ctl = NULL;
 
@@ -1170,7 +1270,7 @@ struct parasite_ctl *compel_prepare_noctx(int pid)
 	ctl->tsock = -1;
 	ctl->ictx.log_fd = -1;
 
-	if (prepare_thread(pid, &ctl->orig))
+	if (prepare_thread(pid, &ctl->orig, handle_rseq))
 		goto err;
 
 	ctl->rpid = pid;
@@ -1346,12 +1446,12 @@ static int make_sigframe_plain(void *from, struct rt_sigframe *f, struct rt_sigf
 	return 0;
 }
 
-struct parasite_ctl *compel_prepare(int pid)
+struct parasite_ctl *compel_prepare(int pid, bool handle_rseq)
 {
 	struct parasite_ctl *ctl;
 	struct infect_ctx *ictx;
 
-	ctl = compel_prepare_noctx(pid);
+	ctl = compel_prepare_noctx(pid, handle_rseq);
 	if (ctl == NULL)
 		goto out;
 

criu/cr-dump.c
@@ -30,6 +30,7 @@
 #include "images/siginfo.pb-c.h"
 
 #include "common/list.h"
+#include "linux/rseq.h"
 #include "imgset.h"
 #include "file-ids.h"
 #include "kcmp-ids.h"

criu/cr-restore.c
@@ -25,6 +25,13 @@
 
 #include "linux/rseq.h"
 
+#ifdef __has_include
+#if __has_include("sys/rseq.h")
+#include <sys/rseq.h>
+#include "asm/thread_pointer.h"
+#endif
+#endif
+
 #include "clone-noasan.h"
 #include "cr_options.h"
 #include "servicefd.h"
@@ -1917,7 +1924,7 @@ static void finalize_restore(void)
 			continue;
 
 		/* Unmap the restorer blob */
-		ctl = compel_prepare_noctx(pid);
+		ctl = compel_prepare_noctx(pid, false);
 		if (ctl == NULL)
 			continue;
 

criu/parasite-syscall.c
@@ -339,6 +339,7 @@ static int make_sigframe(void *arg, struct rt_sigframe *sf, struct rt_sigframe *
 
 static int parasite_prepare_threads(struct parasite_ctl *ctl, struct pstree_item *item)
 {
+	bool handle_rseq = kdat.has_ptrace_get_rseq_conf;
 	struct parasite_thread_ctl **thread_ctls;
 	uint64_t *thread_sp;
 	int i;
@@ -359,7 +360,7 @@ static int parasite_prepare_threads(struct parasite_ctl *ctl, struct pstree_item
 			continue;
 		}
 
-		thread_ctls[i] = compel_prepare_thread(ctl, tid->real);
+		thread_ctls[i] = compel_prepare_thread(ctl, tid->real, handle_rseq);
 		if (!thread_ctls[i])
 			goto free_sp;
 
@@ -380,6 +381,7 @@ free_ctls:
 
 struct parasite_ctl *parasite_infect_seized(pid_t pid, struct pstree_item *item, struct vm_area_list *vma_area_list)
 {
+	bool handle_rseq = kdat.has_ptrace_get_rseq_conf;
 	struct parasite_ctl *ctl;
 	struct infect_ctx *ictx;
 	unsigned long p;
@@ -393,7 +395,7 @@ struct parasite_ctl *parasite_infect_seized(pid_t pid, struct pstree_item *item,
 		return NULL;
 	}
 
-	ctl = compel_prepare_noctx(pid);
+	ctl = compel_prepare_noctx(pid, handle_rseq);
 	if (!ctl)
 		return NULL;
 

compel/test/fdspy/spy.c
@@ -39,7 +39,7 @@ static int do_infection(int pid, int *stolen_fd)
 		err_and_ret("Can't stop task");
 
 	printf("Preparing parasite ctl\n");
-	ctl = compel_prepare(pid);
+	ctl = compel_prepare(pid, false);
 	if (!ctl)
 		err_and_ret("Can't prepare for infection");
 

compel/test/infect/spy.c
@@ -38,7 +38,7 @@ static int do_infection(int pid)
 		err_and_ret("Can't stop task");
 
 	printf("Preparing parasite ctl\n");
-	ctl = compel_prepare(pid);
+	ctl = compel_prepare(pid, false);
 	if (!ctl)
 		err_and_ret("Can't prepare for infection");
 

compel/test/rsys/spy.c
@@ -33,7 +33,7 @@ static int do_rsetsid(int pid)
 		err_and_ret("Can't stop task");
 
 	printf("Preparing parasite ctl\n");
-	ctl = compel_prepare(pid);
+	ctl = compel_prepare(pid, false);
 	if (!ctl)
 		err_and_ret("Can't prepare for infection");
 

commit 18884d8890958d9dcfe2863c89f8b89d314f8f42

compel/rseq: Fix abort before clearing rseq_cs
compel clears rseq->rseq_cs in the tracee before parasite code runs. If
dump fails after rseq_cs is cleared but before the prepared registers are
restored, a thread can be detached and resumed at its original IP inside
the critical section. The critical section then runs with rseq disabled.

Apply the abort IP to the live stopped task before clearing rseq_cs, so
all unwind paths resume from the abort handler. Mirror the kernel abort
checks by verifying that abort_ip is in the target task address space and
that the registered signature is present at abort_ip - 4.

Add _opts() prepare helpers with COMPEL_PREPARE_HANDLE_RSEQ and keep the
old libcompel entry points as wrappers. This lets CRIU opt into rseq
handling without forcing external users to pass a new boolean argument.

Add rseq03.desc and leave restore_rseq_cs() documented as compatibility
code for older images.

Fixes: 67887251e ("compel: avoid rseq critical sections when executing parasite code")
Signed-off-by: Radostin Stoyanov <rstoyanov@fedoraproject.org>

compel/arch/x86/src/lib/infect.c
@@ -4,8 +4,11 @@
 #include <sys/mman.h>
 #include <sys/user.h>
 #include <errno.h>
+#include <fcntl.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <unistd.h>
 
 #include <compel/asm/fpu.h>
 
@@ -683,6 +686,8 @@ int ptrace_set_regs(pid_t pid, user_regs_struct_t *regs)
  * higher limit, because it's backward compatible.
  */
 #define TASK_SIZE_IA32 (0xffffe000)
+#define TASK_SIZE_IA32_3G 0xc0000000UL
+#define ADDR_LIMIT_3GB 0x08000000
 
 unsigned long compel_task_size(void)
 {
@@ -703,6 +708,58 @@ unsigned long compel_task_size(void)
 		return TASK_SIZE_56;
 
 	return TASK_SIZE_47;
+}
+
+static int get_task_personality(pid_t pid, unsigned long *personality)
+{
+	char path[64], buf[32];
+	int fd, ret;
+	char *end;
+
+	snprintf(path, sizeof(path), "/proc/%d/personality", pid);
+	fd = open(path, O_RDONLY);
+	if (fd < 0) {
+		pr_perror("Can't open %s", path);
+		return -1;
+	}
+
+	ret = read(fd, buf, sizeof(buf) - 1);
+	if (ret < 0) {
+		pr_perror("Can't read %s", path);
+		close(fd);
+		return -1;
+	}
+	close(fd);
+
+	buf[ret] = '\0';
+	errno = 0;
+	*personality = strtoul(buf, &end, 16);
+	if (errno) {
+		pr_perror("Can't parse %s", path);
+		return -1;
+	}
+	if (end == buf) {
+		pr_err("Can't parse %s\n", path);
+		return -1;
+	}
+
+	return 0;
+}
+
+int compel_task_size_for_regs(pid_t pid, user_regs_struct_t *regs, unsigned long *task_size)
+{
+	unsigned long personality;
+
+	if (user_regs_native(regs)) {
+		*task_size = compel_task_size();
+		return 0;
+	}
+
+	if (get_task_personality(pid, &personality))
+		return -1;
+
+	*task_size = (personality & ADDR_LIMIT_3GB) ? TASK_SIZE_IA32_3G : TASK_SIZE_IA32;
+	return 0;
 }
 
 bool __compel_shstk_enabled(user_fpregs_struct_t *ext_regs)

compel/include/infect-priv.h
@@ -77,4 +77,5 @@ extern int sigreturn_prep_regs_plain(struct rt_sigframe *sigframe, user_regs_str
 				     user_fpregs_struct_t *fpregs);
 extern int sigreturn_prep_fpu_frame_plain(struct rt_sigframe *sigframe, struct rt_sigframe *rsigframe);
 extern int compel_execute_syscall(struct parasite_ctl *ctl, user_regs_struct_t *regs, const char *code_syscall);
+extern int __must_check compel_task_size_for_regs(pid_t pid, user_regs_struct_t *regs, unsigned long *task_size);
 #endif

compel/include/uapi/infect.h
@@ -47,12 +47,18 @@ extern int compel_resume_task_sig(pid_t pid, int orig_state, int state, int stop
 struct parasite_ctl;
 struct parasite_thread_ctl;
 
-extern struct parasite_ctl __must_check *compel_prepare(int pid, bool handle_rseq);
-extern struct parasite_ctl __must_check *compel_prepare_noctx(int pid, bool handle_rseq);
+#define COMPEL_PREPARE_HANDLE_RSEQ 0x1
+
+extern struct parasite_ctl __must_check *compel_prepare(int pid);
+extern struct parasite_ctl __must_check *compel_prepare_opts(int pid, unsigned int flags);
+extern struct parasite_ctl __must_check *compel_prepare_noctx(int pid);
+extern struct parasite_ctl __must_check *compel_prepare_noctx_opts(int pid, unsigned int flags);
 extern int __must_check compel_infect(struct parasite_ctl *ctl, unsigned long nr_threads, unsigned long args_size);
 extern int __must_check compel_infect_no_daemon(struct parasite_ctl *ctl, unsigned long nr_threads,
 						unsigned long args_size);
-extern struct parasite_thread_ctl __must_check *compel_prepare_thread(struct parasite_ctl *ctl, int pid, bool handle_rseq);
+extern struct parasite_thread_ctl __must_check *compel_prepare_thread(struct parasite_ctl *ctl, int pid);
+extern struct parasite_thread_ctl __must_check *compel_prepare_thread_opts(struct parasite_ctl *ctl, int pid,
+									  unsigned int flags);
 extern void compel_release_thread(struct parasite_thread_ctl *);
 
 extern int __must_check compel_start_daemon(struct parasite_ctl *ctl);

compel/src/lib/infect.c
@@ -1112,13 +1112,13 @@ int compel_infect(struct parasite_ctl *ctl, unsigned long nr_threads, unsigned l
 	return 0;
 }
 
-struct parasite_thread_ctl *compel_prepare_thread(struct parasite_ctl *ctl, int pid, bool handle_rseq)
+struct parasite_thread_ctl *compel_prepare_thread_opts(struct parasite_ctl *ctl, int pid, unsigned int flags)
 {
 	struct parasite_thread_ctl *tctl;
 
 	tctl = xmemalign(__alignof__(*tctl), sizeof(*tctl));
 	if (tctl) {
-		if (prepare_thread(pid, &tctl->th, handle_rseq)) {
+		if (prepare_thread(pid, &tctl->th, flags & COMPEL_PREPARE_HANDLE_RSEQ)) {
 			xfree(tctl);
 			tctl = NULL;
 		} else {
@@ -1130,9 +1130,85 @@ struct parasite_thread_ctl *compel_prepare_thread(struct parasite_ctl *ctl, int 
 	return tctl;
 }
 
+struct parasite_thread_ctl *compel_prepare_thread(struct parasite_ctl *ctl, int pid)
+{
+	return compel_prepare_thread_opts(ctl, pid, 0);
+}
+
+int __attribute__((weak)) compel_task_size_for_regs(pid_t pid, user_regs_struct_t *regs, unsigned long *task_size)
+{
+	(void)pid;
+	(void)regs;
+
+	*task_size = compel_task_size();
+	return 0;
+}
+
 static bool task_in_rseq(struct criu_rseq_cs *rseq_cs, uint64_t addr)
 {
-	return addr >= rseq_cs->start_ip && addr < rseq_cs->start_ip + rseq_cs->post_commit_offset;
+	return addr - rseq_cs->start_ip < rseq_cs->post_commit_offset;
+}
+
+/*
+ * rseq abort signatures are 32-bit values at abort_ip - 4, so they can be
+ * unaligned and cannot be read with ptrace_peek_area().
+ */
+static int ptrace_peek_u32(pid_t pid, uint64_t addr, uint32_t *val)
+{
+	uint64_t aligned = addr & ~(uint64_t)(sizeof(long) - 1);
+	size_t off = addr - aligned;
+	unsigned long word[2] = {};
+	int ret, old_errno = errno;
+
+	errno = 0;
+	word[0] = (unsigned long)ptrace(PTRACE_PEEKDATA, pid, (void *)(uintptr_t)aligned, NULL);
+	if (word[0] == (unsigned long)-1 && errno) {
+		ret = -errno;
+		pr_perror("PEEKDATA failed");
+		return ret;
+	}
+
+	if (off + sizeof(*val) > sizeof(long)) {
+		errno = 0;
+		word[1] = (unsigned long)ptrace(PTRACE_PEEKDATA, pid, (void *)(uintptr_t)(aligned + sizeof(long)), NULL);
+		if (word[1] == (unsigned long)-1 && errno) {
+			ret = -errno;
+			pr_perror("PEEKDATA failed");
+			return ret;
+		}
+	}
+
+	memcpy(val, (char *)word + off, sizeof(*val));
+	errno = old_errno;
+	return 0;
+}
+
+static int validate_rseq_abort_ip(pid_t pid, user_regs_struct_t *regs, struct __ptrace_rseq_configuration *rseqc,
+				  struct criu_rseq_cs *rseq_cs)
+{
+	uint32_t sig;
+	uint64_t abort_ip = rseq_cs->abort_ip;
+	unsigned long task_size;
+
+	if (compel_task_size_for_regs(pid, regs, &task_size))
+		return -1;
+
+	if (abort_ip >= task_size || abort_ip < sizeof(sig)) {
+		pr_err("invalid rseq abort_ip %#llx\n", (unsigned long long)abort_ip);
+		return -1;
+	}
+
+	if (ptrace_peek_u32(pid, abort_ip - sizeof(sig), &sig)) {
+		pr_err("failed to read rseq abort signature\n");
+		return -1;
+	}
+
+	if (sig != rseqc->signature) {
+		pr_err("invalid rseq abort signature: %#x != %#x\n", sig, rseqc->signature);
+		return -1;
+	}
+
+	return 0;
 }
 
 static int read_rseq_cs(pid_t tid, struct __ptrace_rseq_configuration *rseqc, struct criu_rseq_cs *rseq_cs,
@@ -1143,7 +1219,7 @@ static int read_rseq_cs(pid_t tid, struct __ptrace_rseq_configuration *rseqc, st
 	if (!rseqc->rseq_abi_pointer)
 		return 0;
 
-	ret = ptrace_peek_area(tid, rseq, (void *)(long)(rseqc->rseq_abi_pointer), sizeof(struct criu_rseq));
+	ret = ptrace_peek_area(tid, rseq, (void *)(uintptr_t)rseqc->rseq_abi_pointer, sizeof(struct criu_rseq));
 	if (ret) {
 		pr_err("ptrace_peek_area(%d, %lx, %lx, %lx): fail to read rseq struct\n", tid, (unsigned long)rseq,
 		       (unsigned long)(rseqc->rseq_abi_pointer), (unsigned long)sizeof(uint64_t));
@@ -1153,7 +1229,7 @@ static int read_rseq_cs(pid_t tid, struct __ptrace_rseq_configuration *rseqc, st
 	if (!rseq->rseq_cs)
 		return 0;
 
-	ret = ptrace_peek_area(tid, rseq_cs, (void *)(long)(rseq->rseq_cs), sizeof(struct criu_rseq_cs));
+	ret = ptrace_peek_area(tid, rseq_cs, (void *)(uintptr_t)rseq->rseq_cs, sizeof(struct criu_rseq_cs));
 	if (ret) {
 		pr_err("ptrace_peek_area(%d, %lx, %lx, %lx): fail to read rseq_cs struct\n", tid,
 		       (unsigned long)rseq_cs, (unsigned long)rseq->rseq_cs,
@@ -1177,7 +1253,10 @@ static int parasite_thread_rseq(int pid, struct thread_ctx *ctx)
 
 	ret = ptrace(PTRACE_GET_RSEQ_CONFIGURATION, pid, sizeof(rseqc), &rseqc);
 	if (ret != sizeof(rseqc)) {
-		pr_perror("ptrace(PTRACE_GET_RSEQ_CONFIGURATION, %d) = %d", pid, ret);
+		if (ret < 0)
+			pr_perror("ptrace(PTRACE_GET_RSEQ_CONFIGURATION, %d)", pid);
+		else
+			pr_err("ptrace(PTRACE_GET_RSEQ_CONFIGURATION, %d) returned unexpected size %d\n", pid, ret);
 		return -1;
 	}
 
@@ -1191,13 +1270,13 @@ static int parasite_thread_rseq(int pid, struct thread_ctx *ctx)
 		 rseqc.signature);
 
 	if (read_rseq_cs(pid, &rseqc, rseq_cs, &rseq))
-		goto err;
+		return -1;
 
 	if (rseq_cs->start_ip) {
-		void *zero_addr = 0;
+		uint64_t zero = 0;
 
 		pr_debug(
-			"fixup_thread_rseq for %d: rseq_cs start_ip = %llx abort_ip = %llx post_commit_offset = %llx flags = %x version = %x; IP = %lx\n",
+			"parasite_thread_rseq for %d: rseq_cs start_ip = %llx abort_ip = %llx post_commit_offset = %llx flags = %x version = %x; IP = %lx\n",
 			pid, rseq_cs->start_ip, rseq_cs->abort_ip, rseq_cs->post_commit_offset, rseq_cs->flags,
 			rseq_cs->version, (unsigned long)REG_IP(ctx->regs));
 
@@ -1206,22 +1285,30 @@ static int parasite_thread_rseq(int pid, struct thread_ctx *ctx)
 			return -1;
 		}
 
-		if (task_in_rseq(rseq_cs, REG_IP(ctx->regs)))
-			SET_REG_IP(ctx->regs, rseq_cs->abort_ip);
+		if (rseq.flags || rseq_cs->flags)
+			pr_warn("deprecated rseq flags are ignored for %d: rseq.flags = %#x rseq_cs.flags = %#x\n",
+				pid, rseq.flags, rseq_cs->flags);
 
-		if (ptrace_poke_area(pid, &zero_addr,
-				     (void *)(long)(rseqc.rseq_abi_pointer) +
+		if (task_in_rseq(rseq_cs, REG_IP(ctx->regs))) {
+			if (validate_rseq_abort_ip(pid, &ctx->regs, &rseqc, rseq_cs))
+				return -1;
+			SET_REG_IP(ctx->regs, rseq_cs->abort_ip);
+			if (ptrace_set_regs(pid, &ctx->regs)) {
+				pr_perror("Can't apply rseq abort registers (pid: %d)", pid);
+				return -1;
+			}
+		}
+
+		if (ptrace_poke_area(pid, &zero,
+				     (void *)(uintptr_t)(rseqc.rseq_abi_pointer) +
 					     offsetof(struct criu_rseq, rseq_cs),
-				     sizeof(zero_addr))) {
+				     sizeof(zero))) {
 			pr_err("ptrace_poke_area(%d) failed to zero out rseq_cs\n", pid);
 			return -1;
 		}
 	}
 
 	return 0;
-
-err:
-	return -1;
 }
 
 static int prepare_thread(int pid, struct thread_ctx *ctx, bool handle_rseq)
@@ -1253,7 +1340,7 @@ void compel_release_thread(struct parasite_thread_ctl *tctl)
 	xfree(tctl);
 }
 
-struct parasite_ctl *compel_prepare_noctx(int pid, bool handle_rseq)
+struct parasite_ctl *compel_prepare_noctx_opts(int pid, unsigned int flags)
 {
 	struct parasite_ctl *ctl = NULL;
 
@@ -1270,7 +1357,7 @@ struct parasite_ctl *compel_prepare_noctx(int pid, bool handle_rseq)
 	ctl->tsock = -1;
 	ctl->ictx.log_fd = -1;
 
-	if (prepare_thread(pid, &ctl->orig, handle_rseq))
+	if (prepare_thread(pid, &ctl->orig, flags & COMPEL_PREPARE_HANDLE_RSEQ))
 		goto err;
 
 	ctl->rpid = pid;
@@ -1282,6 +1369,11 @@ struct parasite_ctl *compel_prepare_noctx(int pid, bool handle_rseq)
 err:
 	xfree(ctl);
 	return NULL;
+}
+
+struct parasite_ctl *compel_prepare_noctx(int pid)
+{
+	return compel_prepare_noctx_opts(pid, 0);
 }
 
 /*
@@ -1446,12 +1538,12 @@ static int make_sigframe_plain(void *from, struct rt_sigframe *f, struct rt_sigf
 	return 0;
 }
 
-struct parasite_ctl *compel_prepare(int pid, bool handle_rseq)
+struct parasite_ctl *compel_prepare_opts(int pid, unsigned int flags)
 {
 	struct parasite_ctl *ctl;
 	struct infect_ctx *ictx;
 
-	ctl = compel_prepare_noctx(pid, handle_rseq);
+	ctl = compel_prepare_noctx_opts(pid, flags);
 	if (ctl == NULL)
 		goto out;
 
@@ -1483,6 +1575,11 @@ err:
 	xfree(ctl);
 	ctl = NULL;
 	goto out;
+}
+
+struct parasite_ctl *compel_prepare(int pid)
+{
+	return compel_prepare_opts(pid, 0);
 }
 
 static bool task_in_parasite(struct parasite_ctl *ctl, user_regs_struct_t *regs)

criu/cr-dump.c
@@ -30,7 +30,6 @@
 #include "images/siginfo.pb-c.h"
 
 #include "common/list.h"
-#include "linux/rseq.h"
 #include "imgset.h"
 #include "file-ids.h"
 #include "kcmp-ids.h"

criu/cr-restore.c
@@ -1816,6 +1816,11 @@ static int restore_rseq_cs(void)
 {
 	struct pstree_item *item;
 
+	/*
+	 * New dumps abort active rseq critical sections before parasite code
+	 * runs and do not save rseq_cs_pointer. Keep this path for images
+	 * produced by older CRIU versions.
+	 */
 	for_each_pstree_item(item) {
 		int i;
 
@@ -1924,7 +1929,7 @@ static void finalize_restore(void)
 			continue;
 
 		/* Unmap the restorer blob */
-		ctl = compel_prepare_noctx(pid, false);
+		ctl = compel_prepare_noctx(pid);
 		if (ctl == NULL)
 			continue;
 

criu/parasite-syscall.c
@@ -339,7 +339,7 @@ static int make_sigframe(void *arg, struct rt_sigframe *sf, struct rt_sigframe *
 
 static int parasite_prepare_threads(struct parasite_ctl *ctl, struct pstree_item *item)
 {
-	bool handle_rseq = kdat.has_ptrace_get_rseq_conf;
+	unsigned int prepare_flags = kdat.has_ptrace_get_rseq_conf ? COMPEL_PREPARE_HANDLE_RSEQ : 0;
 	struct parasite_thread_ctl **thread_ctls;
 	uint64_t *thread_sp;
 	int i;
@@ -360,7 +360,7 @@ static int parasite_prepare_threads(struct parasite_ctl *ctl, struct pstree_item
 			continue;
 		}
 
-		thread_ctls[i] = compel_prepare_thread(ctl, tid->real, handle_rseq);
+		thread_ctls[i] = compel_prepare_thread_opts(ctl, tid->real, prepare_flags);
 		if (!thread_ctls[i])
 			goto free_sp;
 
@@ -381,7 +381,7 @@ free_ctls:
 
 struct parasite_ctl *parasite_infect_seized(pid_t pid, struct pstree_item *item, struct vm_area_list *vma_area_list)
 {
-	bool handle_rseq = kdat.has_ptrace_get_rseq_conf;
+	unsigned int prepare_flags = kdat.has_ptrace_get_rseq_conf ? COMPEL_PREPARE_HANDLE_RSEQ : 0;
 	struct parasite_ctl *ctl;
 	struct infect_ctx *ictx;
 	unsigned long p;
@@ -395,7 +395,7 @@ struct parasite_ctl *parasite_infect_seized(pid_t pid, struct pstree_item *item,
 		return NULL;
 	}
 
-	ctl = compel_prepare_noctx(pid, handle_rseq);
+	ctl = compel_prepare_noctx_opts(pid, prepare_flags);
 	if (!ctl)
 		return NULL;
 

compel/test/fdspy/spy.c
@@ -39,7 +39,7 @@ static int do_infection(int pid, int *stolen_fd)
 		err_and_ret("Can't stop task");
 
 	printf("Preparing parasite ctl\n");
-	ctl = compel_prepare(pid, false);
+	ctl = compel_prepare(pid);
 	if (!ctl)
 		err_and_ret("Can't prepare for infection");
 

compel/test/infect/spy.c
@@ -38,7 +38,7 @@ static int do_infection(int pid)
 		err_and_ret("Can't stop task");
 
 	printf("Preparing parasite ctl\n");
-	ctl = compel_prepare(pid, false);
+	ctl = compel_prepare(pid);
 	if (!ctl)
 		err_and_ret("Can't prepare for infection");
 

compel/test/rsys/spy.c
@@ -33,7 +33,7 @@ static int do_rsetsid(int pid)
 		err_and_ret("Can't stop task");
 
 	printf("Preparing parasite ctl\n");
-	ctl = compel_prepare(pid, false);
+	ctl = compel_prepare(pid);
 	if (!ctl)
 		err_and_ret("Can't prepare for infection");
 

compel/test/stack/spy.c
@@ -123,7 +123,7 @@ static int do_infection(int pid)
 		err_and_ret("Can't stop task\n");
 
 	printf("Preparing parasite ctl\n");
-	ctl = compel_prepare(pid, false);
+	ctl = compel_prepare(pid);
 	if (!ctl)
 		err_and_ret("Can't prepare for infection\n");
 

test/zdtm/transition/rseq03.c
@@ -151,6 +151,8 @@ static unsigned long mmap_min_addr = 0x10000UL;
  *   0x14: 48 c7 07 00 00 00 00 movq   $0,(%rdi)     // clear __rseq_abi.rseq_cs on exit
  *   0x1b: c7 01 00 00 00 00    movl   $0,(%rcx)     // set rseq_state = 0
  *   0x21: c3                   ret                  // return
+ *
+ * Keep relative jump offsets in sync with the instruction sizes when editing.
  */
 static const uint8_t test_go_rseq_code[] = {
 	0x48, 0x89, 0x37,

test/zdtm/transition/rseq03.desc
@@ -1,0 +1,1 @@
+{'flavor': 'h', 'arch': 'x86_64', 'feature': 'get_rseq_conf'}