rst0git/criu

commit a001b86c79792a980f9509f419c05a1491bcfa9a

plugins: pass plugin options as argv
Plugins are loaded after CRIU parses command-line and RPC options. A
plugin therefore cannot add its own parser without changing CRIU for
every new setting.

Keep namespaced PLUGIN.NAME=VALUE strings in order and expose them as
an argv array. This lets plugins use getopt_long() and ignore options
they do not recognize. Append RPC and libcriu values instead of
replacing matching names, and clear request values between service
requests.

Co-developed-by: Andrei Vagin <avagin@google.com>
Assisted-by: Jetski:Gemini
Signed-off-by: Radostin Stoyanov <rstoyanov@fedoraproject.org>
Signed-off-by: Andrei Vagin <avagin@google.com>

criu/config.c
@@ -31,6 +31,7 @@
 #include "sockets.h"
 #include "tty.h"
 #include "version.h"
+#include "plugin.h"
 
 #include "common/xmalloc.h"
 
@@ -410,8 +411,9 @@ static int pre_parse(int argc, char **argv, bool *usage_error, bool *no_default_
 	return 0;
 }
 
-void init_opts(void)
+int init_opts(void)
 {
+	cr_plugin_options_free();
 	memset(&opts, 0, sizeof(opts));
 
 	/* Default options */
@@ -437,6 +439,10 @@ void init_opts(void)
 	opts.network_lock_method = NETWORK_LOCK_DEFAULT;
 	opts.ghost_fiemap = FIEMAP_DEFAULT;
 	opts.decompress_threads = 1;
+
+	if (cr_plugin_options_init())
+		return -1;
+	return 0;
 }
 
 bool deprecated_ok(char *what)
@@ -760,10 +766,11 @@ int parse_options(int argc, char **argv, bool *usage_error, bool *has_exec_cmd, 
 		BOOL_OPT("unprivileged", &opts.unprivileged),
 		BOOL_OPT("ghost-fiemap", &opts.ghost_fiemap),
 		BOOL_OPT(OPT_ALLOW_UPROBES, &opts.allow_uprobes),
-		{ "compress",                no_argument,       0, 'c'  },
-		{ "compress-acceleration",   required_argument, 0, 1102 },
-		{ "compress-block",          required_argument, 0, 1103 },
-		{ "decompress-threads",      required_argument, 0, 1104 },
+		{ "compress", no_argument, 0, 'c' },
+		{ "compress-acceleration", required_argument, 0, 1102 },
+		{ "compress-block", required_argument, 0, 1103 },
+		{ "decompress-threads", required_argument, 0, 1104 },
+		{ "plugin-option", required_argument, 0, 1105 },
 		{},
 	};
 
@@ -918,6 +925,10 @@ int parse_options(int argc, char **argv, bool *usage_error, bool *has_exec_cmd, 
 			opts.decompress_threads = (unsigned int)n;
 			break;
 		}
+		case 1105:
+			if (cr_plugin_option_add_arg(optarg))
+				return 1;
+			break;
 		case 1043: {
 			int fd;
 

criu/cr-service.c
@@ -42,6 +42,7 @@
 #include "common/scm.h"
 #include "uffd.h"
 #include "pidfd-store.h"
+#include "plugin.h"
 
 #include "setproctitle.h"
 
@@ -406,6 +407,9 @@ static int setup_opts_from_req(int sk, CriuOpts *req)
 	bool imgs_changed_by_rpc_conf = false;
 	int i;
 	bool dummy = false;
+
+	/* Drop options from a previous request while preserving config defaults. */
+	cr_plugin_options_clear_request();
 
 	if (getsockopt(sk, SOL_SOCKET, SO_PEERCRED, &ids, &ids_len)) {
 		pr_perror("Can't get socket options");
@@ -803,6 +807,11 @@ static int setup_opts_from_req(int sk, CriuOpts *req)
 
 		xfree(tmp_output);
 		xfree(tmp_work);
+	}
+
+	for (i = 0; i < req->n_plugin_options; i++) {
+		if (!req->plugin_options[i] || cr_plugin_option_add_arg(req->plugin_options[i]))
+			goto err;
 	}
 
 	if (resolve_images_dir_path(images_dir_path, imgs_changed_by_rpc_conf, req, ids.pid) < 0)
@@ -1674,7 +1683,8 @@ int cr_service(bool daemon_mode)
 				exit(1);
 
 			close(server_fd);
-			init_opts();
+			if (init_opts())
+				exit(1);
 			ret = cr_service_work(sk);
 			close(sk);
 			exit(ret != 0);

criu/crtools.c
@@ -150,7 +150,8 @@ int main(int argc, char *argv[], char *envp[])
 	if (argc < 2)
 		goto usage;
 
-	init_opts();
+	if (init_opts())
+		return 1;
 
 	ret = parse_options(argc, argv, &usage_error, &has_exec_cmd, state);
 
@@ -162,6 +163,8 @@ int main(int argc, char *argv[], char *envp[])
 		pr_err("command is required\n");
 		goto usage;
 	}
+
+	cr_plugin_default_options_parsed();
 
 	log_set_loglevel(opts.log_level);
 
@@ -459,6 +462,7 @@ usage:
 	       "  -j|--" OPT_SHELL_JOB "        allow one to dump and restore shell jobs\n"
 	       "  -l|--" OPT_FILE_LOCKS "       handle file locks, for safety, only used for container\n"
 	       "  -L|--libdir           path to a plugin directory (by default " CR_PLUGIN_DEFAULT ")\n"
+	       "  --plugin-option=P.N[=V] pass an option to a plugin\n"
 	       "  --timeout NUM         a timeout (in seconds) on collecting tasks during dump\n"
 	       "                        (default 10 seconds)\n"
 	       "  --force-irmap         force resolving names for inotify/fsnotify watches\n"

criu/include/cr_options.h
@@ -124,6 +124,7 @@ struct irmap_path_opt {
 	struct irmap *ir;
 };
 
+
 enum criu_mode {
 	CR_UNSET = 0,
 	CR_DUMP,
@@ -168,6 +169,10 @@ struct cr_options {
 	struct list_head inherit_fds;
 	struct list_head external;
 	struct list_head join_ns;
+	char **plugin_options;
+	int plugin_options_default_n;
+	int plugin_options_n;
+	int plugin_options_max;
 	char *libdir;
 	int use_page_server;
 	unsigned short port;
@@ -299,6 +304,6 @@ extern char *rpc_cfg_file;
 
 extern int parse_options(int argc, char **argv, bool *usage_error, bool *has_exec_cmd, int state);
 extern int check_options(void);
-extern void init_opts(void);
+extern int init_opts(void);
 
 #endif /* __CR_OPTIONS_H__ */

criu/plugin-options.c
@@ -1,0 +1,127 @@
+#include <errno.h>
+#include <limits.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <string.h>
+
+#include "cr_options.h"
+#include "common/xmalloc.h"
+#include "log.h"
+#include "plugin.h"
+
+static char *plugin_argv0 = "criu-plugin";
+
+int cr_plugin_options_init(void)
+{
+	opts.plugin_options_default_n = 1;
+	opts.plugin_options_n = 1;
+	opts.plugin_options_max = 2;
+	opts.plugin_options = xmalloc(sizeof(opts.plugin_options[0]) * opts.plugin_options_max);
+	if (!opts.plugin_options)
+		return -1;
+	opts.plugin_options[0] = plugin_argv0;
+	opts.plugin_options[1] = NULL;
+	return 0;
+}
+
+void cr_plugin_default_options_parsed(void)
+{
+	opts.plugin_options_default_n = opts.plugin_options_n;
+}
+
+void cr_plugin_options_clear(void)
+{
+	int i;
+
+	for (i = 1; i < opts.plugin_options_n; i++)
+		xfree(opts.plugin_options[i]);
+
+	opts.plugin_options_default_n = 1;
+	opts.plugin_options_n = 1;
+	opts.plugin_options[opts.plugin_options_n] = NULL;
+}
+
+void cr_plugin_options_clear_request(void)
+{
+	int i;
+
+	for (i = opts.plugin_options_default_n; i < opts.plugin_options_n; i++)
+		xfree(opts.plugin_options[i]);
+
+	opts.plugin_options_n = opts.plugin_options_default_n;
+	opts.plugin_options[opts.plugin_options_n] = NULL;
+}
+
+void cr_plugin_options_free(void)
+{
+	int i;
+
+	if (!opts.plugin_options)
+		return;
+
+	for (i = 1; i < opts.plugin_options_n; i++)
+		xfree(opts.plugin_options[i]);
+
+	xfree(opts.plugin_options);
+	opts.plugin_options = NULL;
+	opts.plugin_options_n = 0;
+	opts.plugin_options_max = 0;
+	opts.plugin_options_default_n = 0;
+}
+
+int cr_plugin_option_add_arg(const char *arg)
+{
+	char *option;
+	const char *dot, *equal;
+	size_t len;
+
+	if (!arg) {
+		pr_err("Plugin option is missing\n");
+		return -1;
+	}
+
+	dot = strchr(arg, '.');
+	equal = strchr(arg, '=');
+	len = strlen(arg);
+	if (arg[0] == '-' || !dot || dot == arg || dot[1] == '\0' || (equal && equal <= dot + 1)) {
+		pr_err("Invalid plugin option '%s' (expected PLUGIN.NAME[=VALUE])\n", arg);
+		return -1;
+	}
+
+	if (len > SIZE_MAX - 3)
+		return -1;
+
+	if (opts.plugin_options_max < opts.plugin_options_n + 2) {
+		char **new_opts;
+		int new_max;
+
+		new_max = opts.plugin_options_max * 2;
+		new_opts = xrealloc(opts.plugin_options, new_max * sizeof(opts.plugin_options[0]));
+		if (!new_opts)
+			return -1;
+
+		opts.plugin_options = new_opts;
+		opts.plugin_options_max = new_max;
+	}
+
+	option = xzalloc(len + 3);
+	if (!option)
+		return -1;
+
+	snprintf(option, len + 3, "--%s", arg);
+	opts.plugin_options[opts.plugin_options_n] = option;
+	opts.plugin_options_n++;
+	opts.plugin_options[opts.plugin_options_n] = NULL;
+
+	return 0;
+}
+
+int criu_plugin_get_options(int *argc, char ***argv)
+{
+	if (!argc || !argv)
+		return -EINVAL;
+
+	*argc = opts.plugin_options_n;
+	*argv = opts.plugin_options;
+	return 0;
+}

criu/unittest/unit.c
@@ -3,6 +3,7 @@
 #include <string.h>
 #include <assert.h>
 #include <dirent.h>
+#include <errno.h>
 #include <unistd.h>
 #include <fcntl.h>
 #include <sched.h>
@@ -15,8 +16,102 @@
 #include "compression.h"
 #include "page.h"
 #include "pagemap.h"
+#include "cr_options.h"
+#include "plugin.h"
 
 int parse_statement(int i, char *line, char **configuration);
+
+static void test_plugin_options(void)
+{
+	bool usage_error = true;
+	bool has_exec_cmd = false;
+	char **plugin_argv;
+	int plugin_argc;
+	char *argv[] = {
+		(char *)"criu",
+		(char *)"--no-default-config",
+		(char *)"--plugin-option",
+		(char *)"example.option=first",
+		(char *)"check",
+		NULL,
+	};
+	assert(init_opts() == 0);
+	assert(parse_options(5, argv, &usage_error, &has_exec_cmd, PARSING_GLOBAL_CONF) == 0);
+	assert(criu_plugin_get_options(&plugin_argc, &plugin_argv) == 0);
+	assert(plugin_argc == 2);
+	assert(!strcmp(plugin_argv[0], "criu-plugin"));
+	assert(!strcmp(plugin_argv[1], "--example.option=first"));
+	assert(plugin_argv[2] == NULL);
+	cr_plugin_options_clear();
+
+	assert(cr_plugin_option_add_arg("example.option=first") == 0);
+	assert(cr_plugin_option_add_arg("example.option=second") == 0);
+	assert(cr_plugin_option_add_arg("example.empty=") == 0);
+	assert(cr_plugin_option_add_arg("example.equals=left=right") == 0);
+	assert(criu_plugin_get_options(&plugin_argc, &plugin_argv) == 0);
+	assert(plugin_argc == 5);
+	assert(!strcmp(plugin_argv[1], "--example.option=first"));
+	assert(!strcmp(plugin_argv[2], "--example.option=second"));
+	assert(!strcmp(plugin_argv[3], "--example.empty="));
+	assert(!strcmp(plugin_argv[4], "--example.equals=left=right"));
+	assert(plugin_argv[5] == NULL);
+	cr_plugin_options_clear();
+
+	assert(cr_plugin_option_add_arg("example.option=config") == 0);
+	cr_plugin_default_options_parsed();
+	assert(cr_plugin_option_add_arg("example.option=request") == 0);
+	assert(criu_plugin_get_options(&plugin_argc, &plugin_argv) == 0);
+	assert(plugin_argc == 3);
+	assert(!strcmp(plugin_argv[1], "--example.option=config"));
+	assert(!strcmp(plugin_argv[2], "--example.option=request"));
+
+	cr_plugin_options_clear_request();
+	assert(criu_plugin_get_options(&plugin_argc, &plugin_argv) == 0);
+	assert(plugin_argc == 2);
+	assert(!strcmp(plugin_argv[1], "--example.option=config"));
+	cr_plugin_options_clear();
+
+	assert(cr_plugin_option_add_arg("missing.option=value") == 0);
+	assert(criu_plugin_get_options(&plugin_argc, &plugin_argv) == 0);
+	assert(plugin_argc == 2);
+	assert(!strcmp(plugin_argv[1], "--missing.option=value"));
+
+	/* Plugin option without an explicit value (flag) */
+	assert(cr_plugin_option_add_arg("example.option") == 0);
+	assert(criu_plugin_get_options(&plugin_argc, &plugin_argv) == 0);
+	assert(plugin_argc == 3);
+	assert(!strcmp(plugin_argv[2], "--example.option"));
+
+	assert(cr_plugin_option_add_arg("example") == -1);
+	assert(cr_plugin_option_add_arg("example.") == -1);
+	assert(cr_plugin_option_add_arg(".option") == -1);
+	assert(cr_plugin_option_add_arg(".option=value") == -1);
+	assert(cr_plugin_option_add_arg("example.=value") == -1);
+	assert(cr_plugin_option_add_arg("--example.option") == -1);
+	assert(cr_plugin_option_add_arg("-example.option") == -1);
+
+	cr_plugin_options_clear();
+	assert(criu_plugin_get_options(&plugin_argc, &plugin_argv) == 0);
+	assert(plugin_argc == 1);
+	assert(!strcmp(plugin_argv[0], "criu-plugin"));
+	assert(plugin_argv[1] == NULL);
+	assert(criu_plugin_get_options(NULL, &plugin_argv) == -EINVAL);
+	assert(criu_plugin_get_options(&plugin_argc, NULL) == -EINVAL);
+
+	/* Test that init_opts() resets plugin options */
+	assert(cr_plugin_option_add_arg("example.option=saved") == 0);
+	cr_plugin_default_options_parsed();
+	assert(criu_plugin_get_options(&plugin_argc, &plugin_argv) == 0);
+	assert(plugin_argc == 2);
+
+	assert(init_opts() == 0);
+	assert(criu_plugin_get_options(&plugin_argc, &plugin_argv) == 0);
+	assert(plugin_argc == 1);
+	assert(!strcmp(plugin_argv[0], "criu-plugin"));
+	assert(plugin_argv[1] == NULL);
+
+	cr_plugin_options_clear();
+}
 
 static void test_pagemap_offset_alignment(void)
 {
@@ -456,6 +551,7 @@ int main(int argc, char *argv[], char *envp[])
 	test_bfd();
 	test_bwrite();
 	test_pagemap_offset_alignment();
+	test_plugin_options();
 
 	i = parse_statement(0, "", configuration);
 	assert(i == 0);

test/others/rpc/Makefile
@@ -1,4 +1,4 @@
-all: test-c test-ps-addr rpc_pb2.py criu
+all: test-c test-ps-addr test-plugin.so rpc_pb2.py criu
 .PHONY: all
 
 CFLAGS += -g -Werror -Wall -I.
@@ -27,7 +27,9 @@ run: all
 	@# The script read.py is used to wait until 'criu service'
 	@# is ready. As 'read -n 1' in some releases has a bug and does
 	@# not read correctly a \0, using python is a workaround.
-	sudo -g '#1000' -u '#1000' -- bash -c "exec 200<>build/status; \
+	sudo -g '#1000' -u '#1000' -- env CRIU_LIBS_DIR="$(CURDIR)" \
+		CRIU_PLUGIN_OPTION_MARKER="$(CURDIR)/build/_marker_plugin_option" \
+		bash -c "exec 200<>build/status; \
 		./criu service -v4 -W build --address criu_service.socket \
 		-d --pidfile pidfile -o service.log --status-fd 200; \
 		$(PYTHON) read.py build/status"
@@ -52,6 +54,9 @@ test-ps-addr: rpc.pb-c.o test-ps-addr.o
 
 test-ps-addr.o: test-ps-addr.c rpc.pb-c.c
 
+test-plugin.so: test-plugin.c ../../../criu/include/criu-plugin.h
+	$(CC) $(CFLAGS) -iquote../../../criu/include -shared -fPIC $< -o $@
+
 rpc_pb2.py: rpc.proto
 	protoc --proto_path=. --python_out=. rpc.proto
 
@@ -59,5 +64,5 @@ rpc.pb-c.c: rpc.proto
 	protoc --proto_path=. --c_out=. rpc.proto
 
 clean:
-	rm -rf build rpc.pb-c.o test-c.o test-c test-ps-addr.o test-ps-addr rpc.pb-c.c rpc.pb-c.h rpc_pb2.py rpc_pb2.pyc criu
+	rm -rf build rpc.pb-c.o test-c.o test-c test-ps-addr.o test-ps-addr test-plugin.so rpc.pb-c.c rpc.pb-c.h rpc_pb2.py rpc_pb2.pyc criu
 .PHONY: clean

test/others/rpc/test-plugin.c
@@ -1,0 +1,105 @@
+#include <errno.h>
+#include <getopt.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "criu-plugin.h"
+
+#define TEST_PLUGIN_NAME  "test-plugin"
+#define TEST_OPTION_NAME  "test-option"
+#define TEST_OPTION_VALUE "test-value"
+#define TEST_LONG_OPTION  TEST_PLUGIN_NAME "." TEST_OPTION_NAME
+
+static bool test_plugin_option_matches(const char *arg, const char *name)
+{
+	size_t len = strlen(name);
+
+	return !strncmp(arg, "--", 2) && !strncmp(arg + 2, name, len) &&
+	       arg[len + 2] == '=';
+}
+
+static int record_test_plugin_option(void)
+{
+	const char *path = getenv("CRIU_PLUGIN_OPTION_MARKER");
+	FILE *marker;
+	int ret = 0;
+
+	if (!path)
+		return 0;
+
+	marker = fopen(path, "w");
+	if (!marker)
+		return -errno;
+	if (fputs(TEST_OPTION_VALUE "\n", marker) == EOF)
+		ret = -EIO;
+	if (fclose(marker) && !ret)
+		ret = -errno;
+
+	return ret;
+}
+
+static int test_plugin_init(int stage)
+{
+	static const struct option options[] = {
+		{ TEST_LONG_OPTION, required_argument, NULL, 't' },
+		{},
+	};
+	char **argv = NULL;
+	char *saved_optarg;
+	const char *value = NULL;
+	int saved_optopt;
+	int saved_opterr;
+	int saved_optind;
+	int argc;
+	int option;
+	int ret = 0;
+
+	(void)stage;
+	ret = criu_plugin_get_options(&argc, &argv);
+	if (ret)
+		return ret;
+
+	saved_optarg = optarg;
+	saved_optopt = optopt;
+	saved_opterr = opterr;
+	saved_optind = optind;
+	opterr = 0;
+	optind = 0;
+	while ((option = getopt_long(argc, argv, ":", options, NULL)) != -1) {
+		switch (option) {
+		case 't':
+			if (test_plugin_option_matches(argv[optind - 1], TEST_LONG_OPTION))
+				value = optarg;
+			break;
+		case '?':
+			/* The option belongs to another plugin. */
+			break;
+		case ':':
+		default:
+			ret = -EINVAL;
+			goto restore_getopt;
+		}
+	}
+restore_getopt:
+	optarg = saved_optarg;
+	optopt = saved_optopt;
+	opterr = saved_opterr;
+	optind = saved_optind;
+
+	if (ret)
+		return ret;
+	if (value && strcmp(value, TEST_OPTION_VALUE))
+		return -EINVAL;
+	if (value)
+		return record_test_plugin_option();
+	return 0;
+}
+
+static void test_plugin_exit(int stage, int ret)
+{
+	(void)stage;
+	(void)ret;
+}
+
+CR_PLUGIN_REGISTER(TEST_PLUGIN_NAME, test_plugin_init, test_plugin_exit)

commit d0819bc400e9af6a68df9239f749a21f2021c644

cr-service: preserve plugin defaults in workers
Service workers reset opts after fork, discarding plugin options parsed
from configuration files and the service command line. Requests then see
only their own plugin options instead of inheriting the service defaults.

Keep plugin arguments private to plugin-options.c and initialize them
once at startup. Worker option resets no longer touch the inherited
arguments, and request setup continues to clear only request options.

Cover the separate lifetimes in the unit tests and require a service
default alongside the RPC test option.

Fixes: a001b86c7 ("plugins: pass plugin options as argv")
Assisted-by: Codex:GPT-6
Signed-off-by: Radostin Stoyanov <rstoyanov@fedoraproject.org>

criu/config.c
@@ -411,9 +411,8 @@ static int pre_parse(int argc, char **argv, bool *usage_error, bool *no_default_
 	return 0;
 }
 
-int init_opts(void)
+void init_opts(void)
 {
-	cr_plugin_options_free();
 	memset(&opts, 0, sizeof(opts));
 
 	/* Default options */
@@ -439,10 +438,6 @@ int init_opts(void)
 	opts.network_lock_method = NETWORK_LOCK_DEFAULT;
 	opts.ghost_fiemap = FIEMAP_DEFAULT;
 	opts.decompress_threads = 1;
-
-	if (cr_plugin_options_init())
-		return -1;
-	return 0;
 }
 
 bool deprecated_ok(char *what)

criu/cr-service.c
@@ -1683,8 +1683,7 @@ int cr_service(bool daemon_mode)
 				exit(1);
 
 			close(server_fd);
-			if (init_opts())
-				exit(1);
+			init_opts();
 			ret = cr_service_work(sk);
 			close(sk);
 			exit(ret != 0);

criu/crtools.c
@@ -150,7 +150,8 @@ int main(int argc, char *argv[], char *envp[])
 	if (argc < 2)
 		goto usage;
 
-	if (init_opts())
+	init_opts();
+	if (cr_plugin_options_init())
 		return 1;
 
 	ret = parse_options(argc, argv, &usage_error, &has_exec_cmd, state);

criu/include/cr_options.h
@@ -169,10 +169,6 @@ struct cr_options {
 	struct list_head inherit_fds;
 	struct list_head external;
 	struct list_head join_ns;
-	char **plugin_options;
-	int plugin_options_default_n;
-	int plugin_options_n;
-	int plugin_options_max;
 	char *libdir;
 	int use_page_server;
 	unsigned short port;
@@ -304,6 +300,6 @@ extern char *rpc_cfg_file;
 
 extern int parse_options(int argc, char **argv, bool *usage_error, bool *has_exec_cmd, int state);
 extern int check_options(void);
-extern int init_opts(void);
+extern void init_opts(void);
 
 #endif /* __CR_OPTIONS_H__ */

criu/plugin-options.c
@@ -4,69 +4,74 @@
 #include <stdio.h>
 #include <string.h>
 
-#include "cr_options.h"
-#include "common/xmalloc.h"
 #include "log.h"
+#include "common/xmalloc.h"
 #include "plugin.h"
+
+/* Service workers inherit these defaults independently of opts. */
+static char **plugin_options;
+static int plugin_options_default_n;
+static int plugin_options_n;
+static int plugin_options_max;
 
 static char *plugin_argv0 = "criu-plugin";
 
 int cr_plugin_options_init(void)
 {
-	opts.plugin_options_default_n = 1;
-	opts.plugin_options_n = 1;
-	opts.plugin_options_max = 2;
-	opts.plugin_options = xmalloc(sizeof(opts.plugin_options[0]) * opts.plugin_options_max);
-	if (!opts.plugin_options)
+	plugin_options_default_n = 1;
+	plugin_options_n = 1;
+	plugin_options_max = 2;
+	plugin_options = xmalloc(sizeof(plugin_options[0]) * plugin_options_max);
+	if (!plugin_options)
 		return -1;
-	opts.plugin_options[0] = plugin_argv0;
-	opts.plugin_options[1] = NULL;
+	plugin_options[0] = plugin_argv0;
+	plugin_options[1] = NULL;
 	return 0;
 }
 
 void cr_plugin_default_options_parsed(void)
 {
-	opts.plugin_options_default_n = opts.plugin_options_n;
+	plugin_options_default_n = plugin_options_n;
 }
 
 void cr_plugin_options_clear(void)
 {
 	int i;
 
-	for (i = 1; i < opts.plugin_options_n; i++)
-		xfree(opts.plugin_options[i]);
+	for (i = 1; i < plugin_options_n; i++)
+		xfree(plugin_options[i]);
 
-	opts.plugin_options_default_n = 1;
-	opts.plugin_options_n = 1;
-	opts.plugin_options[opts.plugin_options_n] = NULL;
+	plugin_options_default_n = 1;
+	plugin_options_n = 1;
+	plugin_options[plugin_options_n] = NULL;
 }
 
 void cr_plugin_options_clear_request(void)
 {
 	int i;
 
-	for (i = opts.plugin_options_default_n; i < opts.plugin_options_n; i++)
-		xfree(opts.plugin_options[i]);
+	for (i = plugin_options_default_n; i < plugin_options_n; i++)
+		xfree(plugin_options[i]);
 
-	opts.plugin_options_n = opts.plugin_options_default_n;
-	opts.plugin_options[opts.plugin_options_n] = NULL;
+	plugin_options_n = plugin_options_default_n;
+	plugin_options[plugin_options_n] = NULL;
 }
 
 void cr_plugin_options_free(void)
 {
 	int i;
 
-	if (!opts.plugin_options)
+	if (!plugin_options)
 		return;
 
-	for (i = 1; i < opts.plugin_options_n; i++)
-		xfree(opts.plugin_options[i]);
+	for (i = 1; i < plugin_options_n; i++)
+		xfree(plugin_options[i]);
 
-	xfree(opts.plugin_options);
-	opts.plugin_options = NULL;
-	opts.plugin_options_n = 0;
-	opts.plugin_options_max = 0;
-	opts.plugin_options_default_n = 0;
+	xfree(plugin_options);
+	plugin_options = NULL;
+	plugin_options_n = 0;
+	plugin_options_max = 0;
+	plugin_options_default_n = 0;
 }
 
 int cr_plugin_option_add_arg(const char *arg)
@@ -91,17 +96,17 @@ int cr_plugin_option_add_arg(const char *arg)
 	if (len > SIZE_MAX - 3)
 		return -1;
 
-	if (opts.plugin_options_max < opts.plugin_options_n + 2) {
+	if (plugin_options_max < plugin_options_n + 2) {
 		char **new_opts;
 		int new_max;
 
-		new_max = opts.plugin_options_max * 2;
-		new_opts = xrealloc(opts.plugin_options, new_max * sizeof(opts.plugin_options[0]));
+		new_max = plugin_options_max * 2;
+		new_opts = xrealloc(plugin_options, new_max * sizeof(plugin_options[0]));
 		if (!new_opts)
 			return -1;
 
-		opts.plugin_options = new_opts;
-		opts.plugin_options_max = new_max;
+		plugin_options = new_opts;
+		plugin_options_max = new_max;
 	}
 
 	option = xzalloc(len + 3);
@@ -109,9 +114,9 @@ int cr_plugin_option_add_arg(const char *arg)
 		return -1;
 
 	snprintf(option, len + 3, "--%s", arg);
-	opts.plugin_options[opts.plugin_options_n] = option;
-	opts.plugin_options_n++;
-	opts.plugin_options[opts.plugin_options_n] = NULL;
+	plugin_options[plugin_options_n] = option;
+	plugin_options_n++;
+	plugin_options[plugin_options_n] = NULL;
 
 	return 0;
 }
@@ -121,7 +126,7 @@ int criu_plugin_get_options(int *argc, char ***argv)
 	if (!argc || !argv)
 		return -EINVAL;
 
-	*argc = opts.plugin_options_n;
-	*argv = opts.plugin_options;
+	*argc = plugin_options_n;
+	*argv = plugin_options;
 	return 0;
 }

criu/unittest/unit.c
@@ -35,7 +35,8 @@ static void test_plugin_options(void)
 		(char *)"check",
 		NULL,
 	};
-	assert(init_opts() == 0);
+	init_opts();
+	assert(cr_plugin_options_init() == 0);
 	assert(parse_options(5, argv, &usage_error, &has_exec_cmd, PARSING_GLOBAL_CONF) == 0);
 	assert(criu_plugin_get_options(&plugin_argc, &plugin_argv) == 0);
 	assert(plugin_argc == 2);
@@ -65,10 +66,28 @@ static void test_plugin_options(void)
 	assert(!strcmp(plugin_argv[1], "--example.option=config"));
 	assert(!strcmp(plugin_argv[2], "--example.option=request"));
 
+	/* Resetting worker options must not discard plugin arguments. */
+	opts.tree_id = 123;
+	init_opts();
+	assert(opts.tree_id == 0);
+	assert(criu_plugin_get_options(&plugin_argc, &plugin_argv) == 0);
+	assert(plugin_argc == 3);
+	assert(!strcmp(plugin_argv[1], "--example.option=config"));
+	assert(!strcmp(plugin_argv[2], "--example.option=request"));
+	assert(plugin_argv[3] == NULL);
+
+	/* Request setup drops only the previous request arguments. */
 	cr_plugin_options_clear_request();
 	assert(criu_plugin_get_options(&plugin_argc, &plugin_argv) == 0);
 	assert(plugin_argc == 2);
 	assert(!strcmp(plugin_argv[1], "--example.option=config"));
+	assert(plugin_argv[2] == NULL);
+	assert(cr_plugin_option_add_arg("example.option=next-request") == 0);
+	cr_plugin_options_clear_request();
+	assert(criu_plugin_get_options(&plugin_argc, &plugin_argv) == 0);
+	assert(plugin_argc == 2);
+	assert(!strcmp(plugin_argv[1], "--example.option=config"));
+	assert(plugin_argv[2] == NULL);
 	cr_plugin_options_clear();
 
 	assert(cr_plugin_option_add_arg("missing.option=value") == 0);
@@ -98,19 +117,20 @@ static void test_plugin_options(void)
 	assert(criu_plugin_get_options(NULL, &plugin_argv) == -EINVAL);
 	assert(criu_plugin_get_options(&plugin_argc, NULL) == -EINVAL);
 
-	/* Test that init_opts() resets plugin options */
+	/* Plugin option cleanup is independent of ordinary option initialization. */
 	assert(cr_plugin_option_add_arg("example.option=saved") == 0);
 	cr_plugin_default_options_parsed();
 	assert(criu_plugin_get_options(&plugin_argc, &plugin_argv) == 0);
 	assert(plugin_argc == 2);
 
-	assert(init_opts() == 0);
+	cr_plugin_options_free();
+	assert(cr_plugin_options_init() == 0);
 	assert(criu_plugin_get_options(&plugin_argc, &plugin_argv) == 0);
 	assert(plugin_argc == 1);
 	assert(!strcmp(plugin_argv[0], "criu-plugin"));
 	assert(plugin_argv[1] == NULL);
 
-	cr_plugin_options_clear();
+	cr_plugin_options_free();
 }
 
 static void test_pagemap_offset_alignment(void)

test/others/rpc/Makefile
@@ -31,6 +31,7 @@ run: all
 		CRIU_PLUGIN_OPTION_MARKER="$(CURDIR)/build/_marker_plugin_option" \
 		bash -c "exec 200<>build/status; \
 		./criu service -v4 -W build --address criu_service.socket \
+		--plugin-option=test-plugin.default=service-value \
 		-d --pidfile pidfile -o service.log --status-fd 200; \
 		$(PYTHON) read.py build/status"
 	rm -f build/status

test/others/rpc/test-plugin.c
@@ -6,10 +6,11 @@
 
 #include "criu-plugin.h"
 
-#define TEST_PLUGIN_NAME  "test-plugin"
-#define TEST_OPTION_NAME  "test-option"
-#define TEST_OPTION_VALUE "test-value"
-#define TEST_LONG_OPTION  TEST_PLUGIN_NAME "." TEST_OPTION_NAME
+#define TEST_PLUGIN_NAME    "test-plugin"
+#define TEST_OPTION_NAME    "test-option"
+#define TEST_OPTION_VALUE   "test-value"
+#define TEST_LONG_OPTION    TEST_PLUGIN_NAME "." TEST_OPTION_NAME
+#define TEST_DEFAULT_OPTION TEST_PLUGIN_NAME ".default"
 
 static bool test_plugin_option_matches(const char *arg, const char *name)
 {
@@ -43,11 +44,13 @@ static int test_plugin_init(int stage)
 {
 	static const struct option options[] = {
 		{ TEST_LONG_OPTION, required_argument, NULL, 't' },
+		{ TEST_DEFAULT_OPTION, required_argument, NULL, 'd' },
 		{},
 	};
 	char **argv = NULL;
 	char *saved_optarg;
 	const char *value = NULL;
+	const char *default_value = NULL;
 	int saved_optopt;
 	int saved_opterr;
 	int saved_optind;
@@ -72,6 +75,10 @@ static int test_plugin_init(int stage)
 			if (test_plugin_option_matches(argv[optind - 1], TEST_LONG_OPTION))
 				value = optarg;
 			break;
+		case 'd':
+			if (test_plugin_option_matches(argv[optind - 1], TEST_DEFAULT_OPTION))
+				default_value = optarg;
+			break;
 		case '?':
 			/* The option belongs to another plugin. */
 			break;
@@ -91,8 +98,11 @@ restore_getopt:
 		return ret;
 	if (value && strcmp(value, TEST_OPTION_VALUE))
 		return -EINVAL;
-	if (value)
+	if (value) {
+		if (!default_value || strcmp(default_value, "service-value"))
+			return -EINVAL;
 		return record_test_plugin_option();
+	}
 	return 0;
 }
 